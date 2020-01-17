26632d492f07f191c5ff7237d2f5536dab0ac291
Ariel Simulevski <ariel@simulevski.at>
2020-01-17 14:17:19 +0100

    Add run2 and run6 automatization

--- start_run2.sh
+++ start_run2.sh
@@ -11,24 +11,24 @@ kubectl wait --for=condition=Ready --timeout=5m pod/kafka-0
 
 helm install rig ./rig/chart
 
-cd src/run1
+cd src/run2
 docker-compose build
 
 chmod +x start_k8s.sh
 ./start_k8s.sh
 
-kubectl wait --for=condition=Ready --timeout=5m "pod/$(kubectl get pods -l 'app=run1-clients-deployment' -o jsonpath='{.items[0].metadata.name}')"
-kubectl wait --for=condition=Ready --timeout=5m "pod/$(kubectl get pods -l 'app=run1-loader-deployment' -o jsonpath='{.items[0].metadata.name}')"
+kubectl wait --for=condition=Ready --timeout=5m "pod/$(kubectl get pods -l 'app=run2-clients-deployment' -o jsonpath='{.items[0].metadata.name}')"
+kubectl wait --for=condition=Ready --timeout=5m "pod/$(kubectl get pods -l 'app=run2-loader-deployment' -o jsonpath='{.items[0].metadata.name}')"
 
 echo "Starting log collection..."
 
 cd ../..
 
 timeout 10m bash <<EOT
-kubectl logs "$(kubectl get pods -l 'app=run1-clients-deployment' -o jsonpath='{.items[0].metadata.name}')" -f > run1.client.log
+kubectl logs "$(kubectl get pods -l 'app=run2-clients-deployment' -o jsonpath='{.items[0].metadata.name}')" -f > run2.client.log
 EOT
 
 helm uninstall rig
 helm uninstall kafka
-helm uninstall run1-clients
-helm uninstall run1-loader
+helm uninstall run2-clients
+helm uninstall run2-loader
--- start_run6.sh
+++ start_run6.sh
@@ -11,24 +11,24 @@ kubectl wait --for=condition=Ready --timeout=5m pod/kafka-0
 
 helm install rig ./rig/chart
 
-cd src/run1
+cd src/run6
 docker-compose build
 
 chmod +x start_k8s.sh
 ./start_k8s.sh
 
-kubectl wait --for=condition=Ready --timeout=5m "pod/$(kubectl get pods -l 'app=run1-clients-deployment' -o jsonpath='{.items[0].metadata.name}')"
-kubectl wait --for=condition=Ready --timeout=5m "pod/$(kubectl get pods -l 'app=run1-loader-deployment' -o jsonpath='{.items[0].metadata.name}')"
+kubectl wait --for=condition=Ready --timeout=5m "pod/$(kubectl get pods -l 'app=run6-clients-deployment' -o jsonpath='{.items[0].metadata.name}')"
+kubectl wait --for=condition=Ready --timeout=5m "pod/$(kubectl get pods -l 'app=run6-loader-deployment' -o jsonpath='{.items[0].metadata.name}')"
 
 echo "Starting log collection..."
 
 cd ../..
 
 timeout 10m bash <<EOT
-kubectl logs "$(kubectl get pods -l 'app=run1-clients-deployment' -o jsonpath='{.items[0].metadata.name}')" -f > run1.client.log
+kubectl logs "$(kubectl get pods -l 'app=run6-clients-deployment' -o jsonpath='{.items[0].metadata.name}')" -f > run6xw.client.log
 EOT
 
 helm uninstall rig
 helm uninstall kafka
-helm uninstall run1-clients
-helm uninstall run1-loader
+helm uninstall run6-clients
+helm uninstall run6-loader
